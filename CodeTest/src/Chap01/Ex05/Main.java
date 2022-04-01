35b05253c6bf5436593783802687b1a083baa7af
Curry5004 <yunjin5004@naver.com>
2022-04-01 17:51:00 +0900

    [홍지환] 5번문제 해결. 리뷰 작성

--- CodeTest/src/Chap01/Ex05/Main.java
+++ CodeTest/src/Chap01/Ex05/Main.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class Main {
   public String solution(String str) {	  
-	  
+//	  개념 포인트.
+//	  좌우에서 헤더가 움직이고 그 헤더를 바탕으로 단어가 스왑되도록 하는것
+//	  그 헤더가 서로의 영역을 넘어가는 순간 이걸 끊어줘서 바뀌는걸 막음
 	  char[] charArr= str.toCharArray();
 	  int lt = 0;
 	  int rt = str.length()-1;
 //	  System.out.println(rt);
 //	  System.out.println(charArr[rt]);
-	  while(rt!=0||lt!=str.length()-1){
+	  while(lt<rt){
 		  if(Character.isAlphabetic(charArr[lt])&&Character.isAlphabetic(charArr[rt])){
 //			  System.out.println("현제 lt="+lt);
 				 char a = ' ';
